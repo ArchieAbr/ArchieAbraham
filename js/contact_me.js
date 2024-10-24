c9bfac15b3be3c57bfd71312fc59c118e54096a2
Archie Abraham <archiepabraham@gmail.com>
2024-10-24 15:48:33 +0100

    added sliding animation to the form

---
+++ js/contact_me.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formContainer = document.querySelector('.contact-container');
+
+    // Apply the animation when the page loads
+    formContainer.classList.add('visible');
+});
